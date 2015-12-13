dc1332d6fad9560dd82c502bc28d3d2af3e80c2e
Ido Nov <idonov@Idos-MacBook-Pro-4.local>
2015-12-13 12:48:19 +0200

    meetconf

--- game.py
+++ game.py
@@ -1,28 +1,54 @@
 from meet import*
-import time
+import math
 
 cells = []
-
-for i in range(50):
-	ball = {"radius": random.randint(0,90), "x":random.randint(0,100),"y":random.randint(0,100), "dy":random.randint(-5,5), "dx":random.randint(-5,5)}
+for i in range(20):
+	ball = {"radius": random.randint(10,40), "x":random.randint(0,100),"y":random.randint(0,100), "dy":random.randint(-3,3), "dx":random.randint(-3,3), "color":"pink"}
 	circle = create_cell(ball)
 	cells.append(circle)
-		
+
+user = {"radius": 20, "x":0 ,"y":0 ,"dy":1 , "dx":1}
+player = create_cell(user)
 
 def borders(cells):
-	h = get_screen_hight()
-	w = get_screen_width()
+	h = get_screen_height()
+	w = get_screen_width()      
+	for cell in cells:	
+		x = cell.xcor()
+		y = cell.ycor()
+		r = cell.get_radius()
+		if x+r > w or x-r < -w:
+			cell.dx = -cell.dx
+		if y+r > h or y-r < -h:
+			cell.dy = -cell.dy
+
+def user(player):
+	x, y = get_user_direction(player)
+	player.set_dx(x)
+	player.set_dy(y)
+
+
+cells.append(player)
+
+def eat(player, cells):
+	x = player.xcor()
+	y = player.ycor()
+	r = player.get_radius()
 	for cell in cells:
-		x = xcor()
-		y = ycor()
-		r = circle.get_radius()
-		if x+r > w:
-			ball.set_dx(-dx)
-		if y+r > h:
-			ball.set_dy(-dy)
+		cellx = cell.xcor()
+		celly = cell.ycor()
+		cellr = cell.get_radius()
+
+		if (math.sqrt(((y - celly)**2) + ((x - cellx)**2))) <r+cellr and cell != player:
+			player.set_radius(r+cellr/2)
+			cell.set_radius(0)
+
+
 
 
 while True:
 	move_cells(cells)
 	borders(cells)
+	user(player)
+	eat(player, cells)
 
