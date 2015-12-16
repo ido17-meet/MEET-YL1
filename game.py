a107e435f4b5329f6a4a0b6ab25abdfcdc83430e
Ido Nov <idonov@Idos-MacBook-Pro-4.local>
2015-12-16 20:06:08 +0200

    last

--- game.py
+++ game.py
@@ -1,14 +1,20 @@
 from meet import*
 import math
+import time
 
+
+
+colors = ["red","pink", "green", "blue", "brown", "maroon"]
 cells = []
 for i in range(20):
-	ball = {"radius": random.randint(10,40), "x":random.randint(0,100),"y":random.randint(0,100), "dy":random.randint(-3,3), "dx":random.randint(-3,3), "color":"pink"}
+	ball = {"radius": random.randint(5,40), "x":random.randint(0,100),"y":random.randint(0,100), "dy":random.randint(-3,3), "dx":random.randint(-3,3), "color":colors[random.randint(0, len(colors)-1)]}
 	circle = create_cell(ball)
 	cells.append(circle)
 
-user = {"radius": 20, "x":0 ,"y":0 ,"dy":1 , "dx":1}
-player = create_cell(user)
+for i in range(30):
+	ball = {"radius": 5, "x":get_random_x(),"y":get_random_y(), "dy":0, "dx":0, "color": ""}
+	circle = create_cell(ball)
+	cells.append(circle)
 
 def borders(cells):
 	h = get_screen_height()
@@ -28,8 +34,6 @@ def user(player):
 	player.set_dy(y)
 
 
-cells.append(player)
-
 def eat(player, cells):
 	x = player.xcor()
 	y = player.ycor()
@@ -39,11 +43,33 @@ def eat(player, cells):
 		celly = cell.ycor()
 		cellr = cell.get_radius()
 
-		if (math.sqrt(((y - celly)**2) + ((x - cellx)**2))) <r+cellr and cell != player:
+		if (math.sqrt(((y - celly)**2) + ((x - cellx)**2))) <r+cellr and cell != player and r>cellr:
 			player.set_radius(r+cellr/2)
-			cell.set_radius(0)
+			cell.goto(get_random_x(),get_random_y())
+				 
+		elif (math.sqrt(((y - celly)**2) + ((x - cellx)**2))) <r+cellr and cell != player:
+			cellr = cellr + r
+			r = 0
+			write("Game Over!", move=False, align="left", font=("Arial", 100, "bold"))
+			time.sleep(3)
+			bye()
+			quit()
+		if r>get_screen_width()*1.5:
+			pencolor("white")
+			goto(0,0)
+			write("you've won!", move=False, align="center", font=("Arial", 100, "bold"))
+			time.sleep(3)
+			bye()
+			quit()
 
+user_dictionary = {"radius": 12, "x":-150 ,"y":10 ,"dy":1 , "dx":1, "color":"orange"}
+player = create_cell(user_dictionary)
+cells.append(player)
 
+def points():
+	clear()
+	goto(-get_screen_width()+20, get_screen_height()-(get_screen_height()/4))
+	write((str(player.get_radius())+" points")[:5],align="left",font=("Arial", 20, "bold"))
 
 
 while True:
@@ -51,4 +77,4 @@ while True:
 	borders(cells)
 	user(player)
 	eat(player, cells)
-
+	points()
